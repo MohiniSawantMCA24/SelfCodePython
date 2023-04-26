8fdd70ddf5002296c4eb347f342018bb16da21e2
Mohini Sawant <119071458+MohiniSawantMCA24@users.noreply.github.com>
2023-04-26 14:57:16 +0530

    Create prog1.py

---
+++ prog1.py
@@ -0,0 +1,7 @@
+#The provided code stub reads and integer, , from STDIN. For all non-negative integers , i<n print i*i :-
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0,n):
+        if(i>=0):
+            print(i*i)
+        
